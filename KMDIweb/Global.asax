e69ba10373807744b51baa9f87f4da5426441675
warren222 <warrenmangaring22@gmail.com>
2020-10-09 17:05:21 +0800

    Add project files.

---
+++ KMDIweb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KMDIweb.MvcApplication" Language="C#" %>
---
+++ KMDIweb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KMDIweb.Startup))]
+namespace KMDIweb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
