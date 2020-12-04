c111985e99914b7a71f2a7c41f03ea1dbe6772f5
warren222 <warrenmangaring22@gmail.com>
2020-12-04 10:20:21 +0800

    fgfg

--- KMDIweb/KMDIweb/Global/mainmenu.aspx.designer.cs
+++ KMDIweb/KMDIweb/Global/mainmenu.aspx.designer.cs
@@ -74,5 +74,23 @@ namespace KMDIweb.KMDIapp {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+        
+        /// <summary>
+        /// Image3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image3;
+        
+        /// <summary>
+        /// LinkButton4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton4;
     }
 }
