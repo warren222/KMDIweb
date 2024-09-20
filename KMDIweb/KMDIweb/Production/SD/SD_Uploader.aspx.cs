a32bacb4ab8521f641875164ab6a129d32c194c1
warren222 <warrenmangaring22@gmail.com>
2024-09-20 15:47:54 +0800

    v

--- KMDIweb/KMDIweb/Production/SD/SD_Uploader.aspx.cs
+++ KMDIweb/KMDIweb/Production/SD/SD_Uploader.aspx.cs
@@ -121,7 +121,7 @@ namespace KMDIweb.KMDIweb.Production.SD
                 model = x.Files_In_Model_Virtual(folder_path);
 
                 FileModel fm = new FileModel();
-                fm = model.Where(m => m.FileName.ToString().Replace(m.FileExtension.ToString(), "") == ddlK_No.SelectedValue.ToString()).SingleOrDefault();
+                fm = model.Where(m => m.FileName.ToString().Replace(m.FileExtension.ToString(), "") == ddlK_No.SelectedValue.ToString()).FirstOrDefault();
 
                 string path = fm == null ? "" : fm.File_Path.ToString();
 
