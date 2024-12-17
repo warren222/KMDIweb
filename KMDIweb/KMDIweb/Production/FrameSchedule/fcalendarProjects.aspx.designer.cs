f157dfc05f531f7514a7faa06db2b60e2e277894
warren222 <warrenmangaring22@gmail.com>
2024-12-17 13:17:41 +0800

    SD

--- KMDIweb/KMDIweb/Production/FrameSchedule/fcalendarProjects.aspx.designer.cs
+++ KMDIweb/KMDIweb/Production/FrameSchedule/fcalendarProjects.aspx.designer.cs
@@ -51,6 +51,15 @@ namespace KMDIweb.KMDIweb.Production.FrameSchedule
         protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
 
         /// <summary>
+        /// lblSelectedAE control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSelectedAE;
+
+        /// <summary>
         /// lblTotalPoints control.
         /// </summary>
         /// <remarks>
