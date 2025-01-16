537b0eea5d04abdce97d3ed7c778c784b9e1a2da
warren222 <warrenmangaring22@gmail.com>
2025-01-16 15:54:38 +0800

    Update DRupdates.aspx.cs

--- KMDIweb/KMDIweb/Delivery/ScannedDR/DRupdates.aspx.cs
+++ KMDIweb/KMDIweb/Delivery/ScannedDR/DRupdates.aspx.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using KMDIweb.SCREENfab;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,69 @@ namespace KMDIweb.KMDIweb.Delivery.ScannedDR
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["KMDI_userid"] != null)
+            {
+                if (!IsPostBack)
+                {
+                    Get_PO();
+                }
+            }
+            else
+            {
+                Response.Redirect("~/KMDIweb/Global/Login.aspx");
+            }
+        }
+        private string sqlconstr
+        {
+            get
+            {
+                return ConnectionString.sqlconstr();
+            }
+        }
+        private void errorrmessage(string message)
+        {
+            CustomValidator err = new CustomValidator();
+            err.ValidationGroup = "errorval";
+            err.IsValid = false;
+            err.ErrorMessage = message;
+            Page.Validators.Add(err);
+        }
+        protected void btnFind_Click(object sender, EventArgs e)
+        {
+            Get_PO();
+        }
+        private void Get_PO()
+        {
+            using (SqlConnection sqlcon = new SqlConnection(sqlconstr))
+            {
+                using (SqlCommand sqlcmd = sqlcon.CreateCommand())
+                {
+                    try
+                    {
+                        sqlcon.Open();
+                        sqlcmd.CommandText = "DR_Web_Updates_Stp";
+                        sqlcmd.CommandType = CommandType.StoredProcedure;
+                        sqlcmd.Parameters.AddWithValue("@Command", "Get_Data");
+                        sqlcmd.Parameters.AddWithValue("@Find", tboxFind.Text);
+                        sqlcmd.Parameters.AddWithValue("@Specification", ddlSpecification.Text);
+                        DataTable tb = new DataTable();
+                        tb.Clear();
+                        using (SqlDataAdapter da = new SqlDataAdapter())
+                        {
+                            da.SelectCommand = sqlcmd;
+                            da.Fill(tb);
+                            //gvFind.DataSource = tb;
+                            //gvFind.DataBind();
+                        }
+                        string row_count = tb.Rows.Count.ToString("N0");
+                      
+                    }
+                    catch (Exception ex)
+                    {
+                        errorrmessage(ex.ToString());
+                    }
+                }
+            }
         }
     }
 }
